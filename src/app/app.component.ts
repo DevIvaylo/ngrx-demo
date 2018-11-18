3da5531f21b65462f286901c9940104f4c4b243e
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-11-18 11:36:33 +0000

    adding methods which dispatching actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+// Notice how the dispatch calls are now being made by instantiating action objects.
+// In the PostActions.EditText we can pass data to the constructor as the payload the gets processed by the reducer function.
+
+import {Component} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+
+import {Post} from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+interface AppState {
+  post: Post;
+
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  post$: Observable<Post>;
+  text: string;   // form input val
+
+  constructor(private store: Store<AppState>) {
+    this.post$ = this.store.select('post');
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
+
 }
